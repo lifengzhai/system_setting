e41f730e1cc67bf09e69e0bb84f04fb382f349ca
koke <ke218@126.com>
2019-05-05 18:47:52 +0800

    Add handleMethodCall

--- ios/Classes/SystemSettingPlugin.m
+++ ios/Classes/SystemSettingPlugin.m
@@ -1,8 +1,23 @@
 #import "SystemSettingPlugin.h"
-#import <system_setting/system_setting-Swift.h>
 
 @implementation SystemSettingPlugin
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftSystemSettingPlugin registerWithRegistrar:registrar];
+    FlutterMethodChannel *channel = [FlutterMethodChannel methodChannelWithName:@"system_setting" binaryMessenger:[registrar messenger]];
+    SystemSettingPlugin *instance = [[SystemSettingPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void) handelMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
+    if ([@"gotoSetting" isEqualToString:call.method]) {
+        NSURL *url = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
+        if ([[UIApplication sharedApplication] canOpenURL:url]) {
+            [[UIApplication sharedApplication] openURL:url];
+            result(nil);
+        }
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
+}
+
 @end
